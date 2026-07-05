693c375f59c1c3f72f890bfd4225ce3d58fb94a5
kongali1720 <121486486+kongali1720@users.noreply.github.com>
2026-07-06 05:09:52 +0700

    Create User.js

---
+++ src/core/models/User.js
@@ -0,0 +1,10 @@
+class User {
+    constructor(id, name, bankAccount, country) {
+        this.id = id;
+        this.name = name;
+        this.bankAccount = bankAccount;
+        this.country = country;
+    }
+}
+
+module.exports = User;
